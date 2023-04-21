5244c6dcebe419489abcc4032565c763f4ba5bcd
Julien RICHARD <julien.y.richard@gmail.com>
2023-04-21 15:28:03 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import Home from './pages/Home';
+import NavBar from './components/Nav';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [lists, setLists] = useState([]);
+  useEffect(() => {
+    axios.get('https://trello.com/b/TXsA6WS9/dynamic-component.json')
+      .then(res => {
+        console.log(res.data)
+        setCards(res.data.cards)
+        setLists(res.data.lists)
+      })
+      .catch(err => console.error(err))
+  },[])
+
+
+
   return (
-    <div className="App">
-      <h1>Dynamic Component with trello Api as Back Office</h1>
-    </div>
+    <BrowserRouter>
+    <NavBar lists={lists} />
+      <Routes>
+        <Route path="/:page" element={<Home cards={cards} lists={lists} />}></Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
